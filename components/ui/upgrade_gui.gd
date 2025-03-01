5e944f0351117bdc33cd704974b44cdd492182ae
heri <69389203+eculipseNamae@users.noreply.github.com>
2025-03-01 17:00:31 +0800

    merge issue

---
+++ components/ui/upgrade_gui.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().paused = false
+	queue_free()
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().paused = false
+	queue_free()
+
+func _on_texture_button_3_pressed() -> void:
+	get_tree().paused = false
+	queue_free()
+
+func _on_texture_button_4_pressed() -> void:
+	get_tree().paused = false
+	queue_free()
