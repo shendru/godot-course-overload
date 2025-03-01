dc119de21284ce0f57eb24e532db75f9013e13f1
heri <69389203+eculipseNamae@users.noreply.github.com>
2025-03-01 16:25:56 +0800

    added upgradeGUI

--- scripts/damage.gd
+++ scripts/damage.gd
@@ -5,6 +5,5 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
 		#var randomDMG = randi_range(1,3)
 		body.call_deferred("take_damage", 1)
-
 func _on_timer_timeout() -> void:
 		queue_free()
